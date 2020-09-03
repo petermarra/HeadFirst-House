f121ba25f67657aa198f50579f2dd142fb8bf626
Peter Marra <neverquit@roadrunner.com>
2020-09-02 21:19:35 -0400

    add code for the Location class create has exterior interfae

--- House/Location.cs
+++ House/Location.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace House
 {
-     abstract class Location
+    abstract class Location
     {
+        public Location(string name)
+        {
+            Name = name;
+        }
+        public Location[] Exits;
+        public string Name { get; private set; }
+        public virtual string Description
+        {
+            get
+            {
+                string description = $"You're standing in the {Name}. " +
+                    $"You see exits  to the following places: ";
+                for (int i = 0; i < Exits.Length; i++)
+                {
+                    description += $" {Exits[i].Name}";
+                    if (i != Exits.Length - 1)
+                        description += ",";
+                }
+                return description;
+            }
+        }
     }
 }
